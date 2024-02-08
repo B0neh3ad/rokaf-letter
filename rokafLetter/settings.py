fb535d8a11f4759960f03cd50cd992062cfe1e21
B0neh3ad <js1044k@gmail.com>
2024-02-08 15:26:42 +0900

    Update settings.py to production env

--- rokafLetter/settings.py
+++ rokafLetter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u6&snqi27p@bf%lr&8=+*msgm7zb(tv)-nkdw)z9r6yh16@0bg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
