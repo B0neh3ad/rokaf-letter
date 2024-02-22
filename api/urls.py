4c916557df1bda2738f6645ebbce87b9086d3ecd
B0neh3ad <js1044k@gmail.com>
2024-02-22 17:32:26 +0900

    Change gpt_test view to class_based POST view

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ router.register(r'trainees', views.TraineeViewSet, basename='trainee')
 router.register(r'letters', views.LetterViewSet, basename='letter_list')
 
 urlpatterns = [
-    path('gpt/test/', views.gpt_test, name='gpt_test'),
+    path('gpt/test/', views.GptTest.as_view(), name='gpt_test'),
     path('trainees/search/', views.TraineeSearchView.as_view(), name='search_trainee'),
     path('', include(router.urls)),
 ]
