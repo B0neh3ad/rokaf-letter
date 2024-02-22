35661d253938a266cba7725d2ba27b0776526954
B0neh3ad <js1044k@gmail.com>
2024-02-22 15:47:35 +0900

    Add permission classes on api views

--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import *
 
 from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 import rokaf_crawler
@@ -30,6 +31,7 @@ load_dotenv(verbose=True)
 """
 
 @require_GET
+@permission_classes([AllowAny, ])
 def gpt_test(request) -> JsonResponse:
     """
     주어진 text prompt에 대하여
@@ -63,6 +65,7 @@ def gpt_test(request) -> JsonResponse:
     return JsonResponse({'content': response})
 
 @require_GET
+@permission_classes([AllowAny, ])
 def search_trainee(request):
     name = request.GET.get('name', None)
     birthday = request.GET.get('birthday', None)
@@ -86,13 +89,14 @@ class TraineeViewSet(mixins.CreateModelMixin,
                    mixins.DestroyModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Trainee.objects.all()
     serializer_class = TraineeSerializer
 
 
-# TODO: 특정 trainee에게 발송된 모든 편지 목록 조회(letters?receiver_id={trainee_id}) (1)
 # TODO: 내가 작성한 편지인 경우 바로 편지 수정/삭제/발송 등이 가능하도록 수정
 class LetterViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Letter.objects.all()
     serializer_class = LetterListSerializer
 
